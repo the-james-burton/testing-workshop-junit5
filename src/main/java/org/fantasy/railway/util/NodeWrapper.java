d0c8fcc9e8462dd2013b0d8ec38acbca732b5755
James Burton <the.james.burton@gmail.com>
2022-06-15 18:37:23 +0100

    added shortest path calculator

---
+++ src/main/java/org/fantasy/railway/util/NodeWrapper.java
@@ -0,0 +1,25 @@
+package org.fantasy.railway.util;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Data structure containing a node, it's total distance from the start and its predecessor.
+ *
+ * @author <a href="sven@happycoders.eu">Sven Woltmann</a>
+ * Simplified with Lombok, moved to inner static class to remove visibility
+ */
+@Data
+@Builder
+class NodeWrapper<N> implements Comparable<NodeWrapper<N>> {
+
+    private final N node;
+    private int totalDistance;
+    private NodeWrapper<N> predecessor;
+
+    @Override
+    public int compareTo(NodeWrapper<N> other) {
+        return Integer.compare(this.totalDistance, other.totalDistance);
+    }
+
+}
