f69b4faf7f4990da6c2b7f963fca4c519f9a5fcc
James Burton <the.james.burton@gmail.com>
2022-07-11 08:57:32 +0100

    changes to Main

--- src/main/java/org/fantasy/railway/Main.java
+++ src/main/java/org/fantasy/railway/Main.java
@@ -1,17 +1,20 @@
 package org.fantasy.railway;
 
-import java.util.Scanner;
+import org.fantasy.railway.ui.RailwayUI;
 
 public class Main {
 
     RailwaySystem system;
     RailwayUI ui;
 
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
         Main main = new Main();
         main.system = new RailwaySystem();
         main.system.initialize();
-        main.ui = new RailwayUI(main.system, System.out);
+        main.ui = new RailwayUI();
+        main.ui.setSystem(main.system);
+        main.ui.setOut(System.out);
+        main.ui.initialize();
         main.ui.topMenu();
     }
 
