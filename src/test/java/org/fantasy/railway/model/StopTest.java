be839c0af1194b05384e8df576a61b2dd28ed9e1
James Burton <the.james.burton@gmail.com>
2022-07-26 11:09:51 +0100

    stop test

---
+++ src/test/java/org/fantasy/railway/model/StopTest.java
@@ -0,0 +1,42 @@
+package org.fantasy.railway.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StopTest {
+
+    Stop emptyStop;
+    Stop firstStop;
+    Stop secondStop;
+
+    @BeforeEach
+    void setup() {
+        emptyStop = Stop.builder().build();
+
+        firstStop = Stop.builder()
+                .station(Station.builder().name("First stop").build())
+                .when(LocalTime.of(10, 0))
+                .build();
+
+        secondStop = Stop.builder()
+                .station(Station.builder().name("Second stop").build())
+                .when(LocalTime.of(10, 5))
+                .build();
+    }
+
+    @Test
+    void shouldHaveDefaultTimeOnCreation() {
+        assertThat(emptyStop.getWhen()).isNotNull();
+        assertThat(emptyStop.getWhen()).isEqualTo(LocalTime.of(0, 0));
+    }
+
+    @Test
+    void shouldSortByWhen() {
+        assertThat(secondStop).isGreaterThan(firstStop);
+    }
+
+}
