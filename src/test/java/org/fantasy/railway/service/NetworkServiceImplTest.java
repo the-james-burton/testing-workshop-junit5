364879692b9546391c96d58056b1e7eb39351a8e
James Burton <the.james.burton@gmail.com>
2022-07-26 16:56:58 +0100

    additional tests

--- src/test/java/org/fantasy/railway/service/NetworkServiceImplTest.java
+++ src/test/java/org/fantasy/railway/service/NetworkServiceImplTest.java
@@ -1,25 +1,44 @@
 package org.fantasy.railway.service;
 
+import org.fantasy.railway.model.Service;
 import org.fantasy.railway.model.Station;
 import org.fantasy.railway.services.NetworkServiceImpl;
+import org.fantasy.railway.services.TimetableService;
+import org.fantasy.railway.util.Now;
+import org.fantasy.railway.util.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ExtendWith(MockitoExtension.class)
 class NetworkServiceImplTest {
 
     NetworkServiceImpl network;
 
+    @Mock
+    TimetableService timetable;
+
     @BeforeEach
     void setup() {
         network = new NetworkServiceImpl();
+        network.setTimetable(timetable);
     }
 
     @Test
@@ -81,7 +100,7 @@ class NetworkServiceImplTest {
         assertThat(network.getItems()).contains(station);
     }
 
-        @Test
+    @Test
     void shouldThrowExceptionIfAddConnectionWithStationNotExist() {
         Station station = Station.builder().name("Test station").build();
         Station connection = Station.builder().name("connection").build();
@@ -115,29 +134,69 @@ class NetworkServiceImplTest {
 
     @Test
     void shouldAddStationsFromStringInput() {
-        String first = "first station";
-        String second = "second station";
-        String third = "third station";
+        String firstName = "first station";
+        String secondName = "second station";
+        String thirdName = "third station";
 
         Integer firstToSecond = 7;
-        Integer secondToThird = 9;
+        Integer firstToThird = 9;
 
         Queue<String> inputs = new LinkedList<>();
-        inputs.add(first);
-        inputs.add(second);
+        inputs.add(firstName);
+        inputs.add(secondName);
         inputs.add(firstToSecond.toString());
-        inputs.add(third);
-        inputs.add(secondToThird.toString());
+        inputs.add(thirdName);
+        inputs.add(firstToThird.toString());
 
         Station station = network.addStation(inputs);
+        Station first = network.getStationOrThrow(firstName);
+        Station second = network.getStationOrThrow(secondName);
+        Station third = network.getStationOrThrow(thirdName);
 
         assertThat(station.getId()).isEqualTo(1);
-        assertThat(station.getName()).isEqualTo(first);
-        assertThat(network.getItems()).contains(
-                network.getStationOrThrow(first),
-                network.getStationOrThrow(second),
-                network.getStationOrThrow(third));
-
+        assertThat(station).isEqualTo(first);
+        assertThat(network.getItems()).contains(first, second, third);
+        assertThat(network.distanceBetweenAdjacent(first, second)).isEqualTo(firstToSecond);
+        assertThat(network.distanceBetweenAdjacent(first, third)).isEqualTo(firstToThird);
     }
 
+    @Test
+    void shouldRemoveStationIfNoService() {
+        Mockito.when(timetable.getServices()).thenReturn(new ArrayList<>());
+        String name = "Test station";
+
+        Station station = network.getStationOrCreate(name);
+        network.removeStation(station);
+
+        assertThat(network.getStation(station.getName())).isNotPresent();
+        Mockito.verify(timetable, Mockito.times(1)).getServices();
+    }
+
+    @Test
+    void shouldNotRemoveStationIfFutureServiceExists() {
+        // create a service that our mock will return...
+        Service service = Service.builder().id(1).build();
+        service.setRoute(TestUtils.createTestRoute(service));
+        service.setName(service.getCurrentName());
+        List<Service> services = Arrays.asList(service);
+
+        Mockito.when(timetable.getServices()).thenReturn(services);
+
+        // set the time to be before the time of the service we just created...
+        Clock clock = Clock.fixed(Instant.parse("2022-05-10T08:00:00Z"), ZoneOffset.UTC);
+        Now.setClock(clock);
+
+        String stationName = service.getRoute().peek().getStation().getName();
+        Station station = network.getStationOrCreate(stationName);
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                network.removeStation(station)
+        );
+
+        String expected = "There is a service stopping at";
+        String actual = exception.getMessage();
+
+        assertThat(actual).startsWith(expected);
+        assertThat(network.getStation(station.getName())).isPresent();
+        Mockito.verify(timetable, Mockito.times(1)).getServices();
+    }
 }
